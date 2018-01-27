5c306b72d6ce506fcc1bc053b53528bcfe287792
Adrian Giełzak <adrian.gielzak@outlook.com>
2018-01-27 14:02:46 +0100

    Next tests

--- src/main/java/com/sdabyd2/programowanie/PTestImp.java
+++ src/main/java/com/sdabyd2/programowanie/PTestImp.java
@@ -1,7 +1,7 @@
 package com.sdabyd2.programowanie;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PTestImp {
 
@@ -15,10 +15,69 @@ public class PTestImp {
     }
 
     public static List<Integer> reverse(List<Integer> numbers) {
-        List<Integer> reverseList = new ArrayList<Integer>();
+        List<Integer> reverseList = new ArrayList<>();
         for (int i = numbers.size() - 1; i >= 0; i--) {
             reverseList.add(numbers.get(i));
         }
         return reverseList;
+        /*
+        Collections.reverse(numbers);
+         */
+    }
+
+    public static boolean isPalindrome(List<String> list) {
+        boolean isPalindrome = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equalsIgnoreCase(list.get(list.size() - 1 - i))) ;
+            isPalindrome = true;
+        }
+        return isPalindrome;
+    }
+
+    public static Object[] removeAt(List<String> list, int k) {
+        List<String> newList = new ArrayList<>(list);
+        String removeObject = newList.remove(k - 1);
+        return new Object[]{newList, removeObject};
+    }
+
+    public static List<String> randomSelect(List<String> strings, int i) {
+        Random random = new Random(System.currentTimeMillis());
+
+        List<String> newListFromRandomSelect = new ArrayList<>();
+        for (int j = 0; j < i; j++) {
+            newListFromRandomSelect.add(strings.get(random.nextInt(random.nextInt(strings.size()))));
+        }
+
+        return newListFromRandomSelect;
+    }
+
+//    public List<String> flatten(List<Object> objects) {
+//        List<String> flattenList = new ArrayList<>();
+//        flattenList = objects.stream()
+//                .flatMap(Collection::stream)
+//                .collect(Collectors.toList());
+//
+//        return flattenList;
+//    }
+
+    public static List<String> compress(List<String> strings) {
+        Set<String> unique = new HashSet<>();
+        List<String> compressedList = new ArrayList<>();
+        for (String s : strings) {
+                unique.add(s);
+        }
+        for (String s : unique) {
+            compressedList.add(s);
+        }
+        return compressedList;
+    }
+
+    public static List<String> duplicate(List<String> strings) {
+        List<String> duplicates = new ArrayList<>();
+        for (String s : strings) {
+            duplicates.add(s);
+            duplicates.add(s);
+        }
+        return duplicates;
     }
 }
--- src/test/java/PTest.java
+++ src/test/java/PTest.java
@@ -1,13 +1,20 @@
 import com.sdabyd2.programowanie.PTestImp;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+//import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertTrue;
 
 public class PTest {
 
@@ -25,6 +32,50 @@ public class PTest {
     public void shouldReverseAList() throws Exception {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         assertThat(PTestImp.reverse(numbers), is(equalTo(Arrays.asList(5, 4, 3, 2, 1))));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenListIsPalindrome() throws Exception {
+        assertTrue(PTestImp.isPalindrome(Arrays.asList("x", "a", "m", "a", "x")));
+    }
+
+    @Test
+    public void shouldRemoveKthElementFromList() throws Exception {
+        Object[] result = PTestImp.removeAt(Arrays.asList("a", "b", "c", "d"), 2);
+        assertThat(result[0], CoreMatchers.<Object>equalTo(Arrays.asList("a", "c", "d")));
+        assertThat(result[1], CoreMatchers.<Object>equalTo("b"));
+    }
+
+    @Test
+    public void shouldReturnAListOfThreeRandomSelectedElements() throws Exception {
+        List<String> result =
+                PTestImp.randomSelect(
+                        Arrays.asList(
+                                "a", "b", "c", "d", "e", "f", "g", "h"), 3);
+        System.out.println(result);
+        assertThat(result, hasSize(3));
+    }
+
+//    @Test
+//    public void shouldFlattenAListOfList() throws Exception {
+//        List<String> flatten = PTestImp.flatten(
+//                asList("a", asList("b", asList("c", "d")), "e"));
+//        assertThat(flatten, hasSize(5));
+//        assertThat(flatten, hasItems("a", "b", "c", "d", "e"));
+//    }
+
+    @Test
+    public void shouldRemoveConsecutiveDuplicatesInAList() throws Exception {
+        List<String> compressedList = PTestImp.compress(asList("a", "a", "a", "a", "b", "c", "c", "d", "e", "e", "e", "e"));
+        assertThat(compressedList, hasSize(5));
+        assertThat(compressedList, contains("a", "b", "c", "d", "e"));
+    }
+
+    @Test
+    public void shouldDuplicateElementsInAList() throws Exception {
+        List<String> duplicates = PTestImp.duplicate(Arrays.asList("a", "b", "c", "d"));
+        assertThat(duplicates, hasSize(8));
+        assertThat(duplicates, contains("a", "a", "b", "b", "c", "c", "d", "d"));
     }
 
 }
