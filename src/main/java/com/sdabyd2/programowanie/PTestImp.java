b9800ae9b710b94e3c2c605a1fd3d84f6c5a0382
Adrian Giełzak <adrian.gielzak@outlook.com>
2018-01-27 11:03:25 +0100

    Size List and Reverse List

--- src/main/java/com/sdabyd2/programowanie/PTestImp.java
+++ src/main/java/com/sdabyd2/programowanie/PTestImp.java
@@ -1,5 +1,6 @@
 package com.sdabyd2.programowanie;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PTestImp {
@@ -7,5 +8,17 @@ public class PTestImp {
 
     public static String last(List<String> lista) {
         return lista.get(lista.size() - 1);
+    }
+
+    public static int length(List<Integer> integers) {
+        return integers.size();
+    }
+
+    public static List<Integer> reverse(List<Integer> numbers) {
+        List<Integer> reverseList = new ArrayList<Integer>();
+        for (int i = numbers.size() - 1; i >= 0; i--) {
+            reverseList.add(numbers.get(i));
+        }
+        return reverseList;
     }
 }
--- src/test/java/PTest.java
+++ src/test/java/PTest.java
@@ -1,6 +1,9 @@
 import com.sdabyd2.programowanie.PTestImp;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 import static java.util.Arrays.asList;
@@ -10,8 +13,18 @@ public class PTest {
 
     @Test
     public void shouldFindLastElementFromList() throws Exception {
-
         assertThat(PTestImp.last(asList("a", "b", "c", "d")), is(equalTo("d")));
-
     }
+
+    @Test
+    public void shouldFindListOfNonEmptyList() throws Exception {
+        assertThat(PTestImp.length(Arrays.asList(1, 2, 3, 4, 5)), is(equalTo(5)));
+    }
+
+    @Test
+    public void shouldReverseAList() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        assertThat(PTestImp.reverse(numbers), is(equalTo(Arrays.asList(5, 4, 3, 2, 1))));
+    }
+
 }
